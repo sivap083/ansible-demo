0136bea8d947bdafd33a0f19b70e84f716b92dc0
Hemajo <92712735+Hemajo@users.noreply.github.com>
2021-11-15 11:44:32 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,10 +1,10 @@
 package hello;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController
 
 @RestController
-public class HelloController {
+public  HelloController {
 
     @RequestMapping("/")
         public String index() {
